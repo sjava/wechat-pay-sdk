49fd1495eb95e28e94b7bb04c03f773e8c184d7d
sjava <zhu.yuanbin@gmail.com>
2024-03-22 00:28:51 +0800

    feat(media.rs): refactor upload_image function and related structures

--- src/sdk/media.rs
+++ src/sdk/media.rs
@@ -5,30 +5,12 @@
 //!
 //! 请求方式: POST
 //! 请求主体类型： multipart/form-data
-use crate::sdk::common::EmptyRequest;
 use crate::{Client, WeChatPayError};
 use reqwest::multipart::{Form, Part};
 use reqwest::{header, Method};
 use serde::Deserialize;
 use serde_json::json;
 use sha2::{Digest, Sha256};
-use url::Url;
-
-#[derive(Debug, Deserialize)]
-pub struct EncryptCertificate {
-  pub algorithm: String,
-  pub nonce: String,
-  pub associated_data: String,
-  pub ciphertext: String,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct CertificateData {
-  pub serial_no: String,
-  pub effective_time: String,
-  pub expire_time: String,
-  pub encrypt_certificate: EncryptCertificate,
-}
 
 /// # [上传图片响应](self) 响应
 #[derive(Debug, Deserialize)]
@@ -39,19 +21,10 @@ pub struct UploadImageResponse {
 impl Client {
   pub async fn upload_image(
     &self,
-    image: &[u8],
+    image: Vec<u8>,
     filename: &str,
   ) -> Result<UploadImageResponse, WeChatPayError> {
-    const MAX_SIZE: usize = 2 * 1024 * 1024;
     const URL: &str = "/v3/merchant/media/upload";
-    // image's size must be less than 2M
-    // if image.len() > MAX_SIZE {
-    //   return Err(PayError::WechatError(format!(
-    //     "Image size ({} bytes) exceeds the maximum allowed size ({} bytes)",
-    //     image.len(),
-    //     MAX_SIZE
-    //   )));
-    // }
 
     // calculate sha256
     let mut hasher = Sha256::new();
@@ -59,59 +32,93 @@ impl Client {
     let hash = hasher.finalize();
     let hash = hex::encode(hash.as_slice());
 
+    let path = format!("https://api.mch.weixin.qq.com{}", URL);
     let meta = json!( {
         "filename": filename,
         "sha256": hash
     });
-
-    let path = format!("https://api.mch.weixin.qq.com{}", URL);
-
-    let mut headers = header::HeaderMap::new();
-    headers.insert(
-      header::CONTENT_TYPE,
-      header::HeaderValue::from_static("multipart/form-data"),
-    );
-    headers.insert(
-      header::ACCEPT,
-      header::HeaderValue::from_str("application/json")?,
-    );
-    headers.insert(
-      header::USER_AGENT,
-      header::HeaderValue::from_str("wechat-pay-sdk-rs")?,
-    );
     let signature = self.request_authorization(&Method::POST, &path, &meta.to_string())?;
-    headers.insert(
-      header::AUTHORIZATION,
-      header::HeaderValue::from_str(&signature)?,
-    );
-    req.headers_mut().insert(
-      "Wechatpay-Serial",
-      header::HeaderValue::from_str(&self.merchant_serial_number)?,
-    );
+    let headers = self.build_header(signature)?;
 
-    let mut json_part_headers = header::HeaderMap::new();
-    json_part_headers.insert(header::CONTENT_TYPE, "application/json".parse().unwrap());
-    let json_part = Part::text(meta.to_string()).headers(json_part_headers);
+    let form = build_form(&meta, image, filename)?;
 
-    let file_part = Part::bytes(image)
-      .file_name(filename.to_string())
-      .mime_str("image/jpeg")?;
-
-    let form = Form::new().part("meta", json_part).part("file", file_part);
-
-    let res = self
+    let response = self
       .client
       .post(&path)
       .headers(headers)
       .multipart(form)
       .send()
       .await?;
+    let status = response.status();
+    let text = response
+      .text()
+      .await
+      .map_err(|_| WeChatPayError::Unknown("Failed to decode response".to_string()))?;
 
-    // Ok(
-    //   self
-    //     .send_request::<EmptyRequest, _>(Method::GET, "/v3/certificates", None, None)
-    //     .await?
-    //     .unwrap(),
-    // )
+    let response = Self::parse_response::<UploadImageResponse>(status, text)
+      .await?
+      .unwrap();
+    Ok(response)
   }
+
+  fn build_header(&self, signature: String) -> Result<header::HeaderMap, WeChatPayError> {
+    let mut headers = header::HeaderMap::new();
+    headers.insert(
+      header::CONTENT_TYPE,
+      header::HeaderValue::from_str("multipart/form-data")?,
+    );
+    headers.insert(
+      header::ACCEPT,
+      header::HeaderValue::from_str("application/json")?,
+    );
+
+    let chrome_agent = "Mozilla/5.0 (Linux; Android 10; Redmi K30 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Mobile Safari/537.36";
+    headers.insert(
+      header::USER_AGENT,
+      header::HeaderValue::from_str(chrome_agent)?,
+    );
+    headers.insert(
+      header::AUTHORIZATION,
+      header::HeaderValue::from_str(&signature)?,
+    );
+    headers.insert(
+      "Wechatpay-Serial",
+      header::HeaderValue::from_str(&self.merchant_serial_number)?,
+    );
+    Ok(headers)
+  }
+}
+
+fn build_form(
+  meta: &serde_json::Value,
+  image: Vec<u8>,
+  filename: &str,
+) -> Result<Form, WeChatPayError> {
+  let mut json_part_headers = header::HeaderMap::new();
+  json_part_headers.insert(
+    header::CONTENT_TYPE,
+    header::HeaderValue::from_str("application/json")?,
+  );
+  let json_part = Part::text(meta.to_string()).headers(json_part_headers);
+
+  // according to the file extension, get the mime
+  let ext = filename
+    .split('.')
+    .last()
+    .ok_or_else(|| WeChatPayError::Unknown("Invalid filename, no extension found".to_string()))?;
+  let mime = match ext {
+    "jpg" | "jpeg" => Ok("image/jpeg"),
+    "png" => Ok("image/png"),
+    "bmp" => Ok("image/bmp"),
+    _ => Err(WeChatPayError::Unknown(
+      "Unsupported image format".to_string(),
+    )),
+  };
+
+  let file_part = Part::bytes(image)
+    .file_name(filename.to_string())
+    .mime_str(mime?)?;
+
+  let form = Form::new().part("meta", json_part).part("file", file_part);
+  Ok(form)
 }
